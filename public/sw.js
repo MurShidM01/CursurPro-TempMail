bdd44b19891ab03a41be38e51ac37767fa4a67f1
MurShidM01 <www.alikhanjalbani@gmail.com>
2025-11-19 00:10:39 +0500

    Domain Updated

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "fpyf8.com",
-    "zoneId": 186400
+    "domain": "3nbf4.com",
+    "zoneId": 10206187
 }
 self.lary = ""
-importScripts('https://fpyf8.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
